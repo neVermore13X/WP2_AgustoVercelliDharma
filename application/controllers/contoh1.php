f1ef123f3875c1611c64a26f8cedec331143d4e3
neVermore13X <agusto.vercelli@gmail.com>
2023-03-17 18:45:04 +0700

    add existing project files to git

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+    class Contoh1 extends CI_Controller
+    {
+        public function index()
+        {
+            echo "<h1>Memperkenalkan</h1>";
+            echo "Nama Saya Agusto Vercelli Dharma
+                saya tinggal di Cilangkap Depok";
+        }
+    }
+?>
